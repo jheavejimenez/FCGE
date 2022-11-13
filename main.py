dabbb16c065e58b44458f174456ba2d68aaf9bc2
jheave <jimenezjheave123@gmail.com>
2022-11-13 14:48:23 +0800

    fix

--- main.py
+++ main.py
@@ -31,13 +31,35 @@ def find_divisible(a, b, k):
     return count
 
 
+"""
+write a function that given non-empty array A consisting of N integers, returns the number of jumps
+after  which the pawn will be outside the array. the function should return -1 if the pawn will never
+jump out of the array.
+"""
+
+
+def solution(A):
+    N = [False] * len(A)
+    pos = 0
+    jumps = 0
+    while pos < len(A):
+        if N[pos]:
+            return -1
+        N[pos] = True
+        pos += A[pos]
+        jumps += 1
+    return jumps
+
+
 def main():
-    # a = [1, 2, 3]
-    a = [1, 3, 6, 4, 1, 2]
+    a = [1, 2, 3]
+    # a = [1, 3, 6, 4, 1, 2]
     # a = [-1, -1, -1, -5]
     # a = [1, 3, 6, 4, 1, 7, 8, 10]
     print(missing_int(a))
-    print(find_divisible(6, 11, 2))
+    # print(find_divisible(6, 11, 2))
+    # a = [2, 3, -1, 1, 3]
+    # print(solution(a))  # the answer should be 4
 
 
 if __name__ == "__main__":
